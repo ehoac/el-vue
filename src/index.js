a55607366a70e7137768f223a57a12b3d71bbc02
caopeihe <caopeihe@topnet.net.cn>
2019-12-11 16:43:28 +0800

    add element ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
+import 'element-ui/lib/theme-chalk/index.css';
 import './assets/style/test.css'
 
 import guid from './util/uuid.js'
 import http from './util/http.js'
+
+Vue.use(ElementUI)
 
 Vue.prototype.$http = http
 
