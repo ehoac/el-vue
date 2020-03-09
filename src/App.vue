8567605353fb79a579916ac06254d97317269a85
caopeihe <caopeihe@topnet.net.cn>
2020-03-09 17:46:56 +0800

    el

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,68 @@
 <template>
-  <div id='app'>
+  <div id="app">
     <el-container>
       <el-header>
-        <div><div class='menu'></div></div>
+        <div>
+          <div class="menu"></div>
+        </div>
       </el-header>
       <el-container>
         <el-main>
           <article>
             <header>{{main}}</header>
             <section>
-              <div class='i-menu'></div>
-              <div class='i-more'></div>
+              <div class="i-menu"></div>
+              <div class="i-more"></div>
             </section>
           </article>
-          
         </el-main>
-        <el-aside width='200px'>
-          {{aside}}
-        </el-aside>
+        <el-aside width="200px">{{aside}}</el-aside>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        header: 'header',
-        aside: 'aside',
-        main: 'main',
-        footer:'footer'
-      }
-    }
+export default {
+  data() {
+    return {
+      header: "header",
+      aside: "aside",
+      main: "main",
+      footer: "footer"
+    };
   }
+};
 </script>
 
 <style lang='scss'>
-  .el-header{
-    background-color: $s-bgc;
-    color: #fff;
+.el-header {
+  background-color: $main-bgc;
+  color: #fff;
+  @include d-flex;
+  & div {
+    align-items: center;
+  }
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  & section {
     @include d-flex;
-    & div{
-      align-items: center;
+    & div {
+      margin: 0 0.2em;
     }
   }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    & section{
-      @include d-flex;
-      & div {
-        margin: 0 .2em;
-      }
-    }
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
 </style>
--- src/util/http.js
+++ src/util/http.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 
 let isPrompt = false
 const Axios = axios.create({
-  baseURL: 'http://127.0.0.1:10001',
-  timeout: 10*1000,
-  headers: {'Content-Type': 'application/json;charset=UTF-8'},
+  baseURL: 'http://127.0.0.1:17000',
+  timeout: 10 * 1000,
+  headers: { 'Content-Type': 'application/json;charset=UTF-8' },
   responseType: 'json',
   withCredentials: false
 })
